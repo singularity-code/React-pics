97f938a190f4350335e999bafbaaa48a749e930e
Chris <neoxrock@gmail.com>
2019-06-19 17:22:59 +1000

    Grid CSS

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+    this.state = { spans: 0 };
+  }
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 150);
+
+    this.setState({ spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
 
     return (
-        <div>
-          <img
-            alt={description}
-            src={urls.regular}
-            />
-        </div>
+      <div style={{ girdRowEnd: `span ${this.state.spans}`}}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
     );
   }
 }
