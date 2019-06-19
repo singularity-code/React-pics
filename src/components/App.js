e36be1e877a61c0ef88da8fa8c821417f1e163f6
Chris <neoxrock@gmail.com>
2019-06-19 16:01:45 +1000

    Async request

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import axios from 'axios';
-import SearchBar from './SearchBar';
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-      //this is using api
-      axios.get('https://api.unsplash.com/search/photos', {
-        params: { query: term },
-        headers: {
-          Authorization: 'Client-ID 84141cf0034e44a3abfc5c3310b51b47075e9759d7e8a2f8a0bc26c39302d878'
-
-        }
-      });
+  async onSearchSubmit(term) {
+    //this is using api
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 84141cf0034e44a3abfc5c3310b51b47075e9759d7e8a2f8a0bc26c39302d878"
+      }
+    });
+    console.log(response.data.results);
   }
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
