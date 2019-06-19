64ac00e2f25afceb23c7204416b8011910c045ac
Chris <neoxrock@gmail.com>
2019-06-19 16:40:24 +1000

    Keys in the List

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  return <div>ImageList</div>
+  const images = props.images.map(({description, id, urls}) => {
+    return <img alt={description} key={id} src={urls.regular}/>
+  });
+  return <div>{images}</div>
 };
 
 export default ImageList;
